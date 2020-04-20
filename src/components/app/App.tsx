e9a892a5ef195467976810986dccec0097615481
Abhishek Piedy <abhishek.piedy@gmail.com>
2020-04-19 22:24:16 -0400

    move app component

---
+++ src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+import { CountryGraphs } from '../country-graphs/country-graphs';
+
+function App() {
+  return (
+    <div className="app">
+      <CountryGraphs />
+    </div>
+  );
+}
+
+export default App;
