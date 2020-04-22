6a19e101b0e8d70d785738b48d85e7c663335f4b
Abhishek Piedy <abhishek.piedy@gmail.com>
2020-04-22 02:15:05 -0400

    create customized select component

--- src/components/location-selector/location-selector.tsx
+++ src/components/location-selector/location-selector.tsx
@@ -1,16 +1,53 @@
-import React from "react";
+import React from 'react';
 
-import { MenuItem, Select } from '@material-ui/core';
+import { MenuItem, Select, InputBase, Theme, withStyles, createStyles } from '@material-ui/core';
+
+function countryToFlag(isoCode: string) {
+  return typeof String.fromCodePoint !== 'undefined'
+    ? isoCode
+        .toUpperCase()
+        .replace(/./g, (char) => String.fromCodePoint(char.charCodeAt(0) + 127397))
+    : isoCode;
+}
+
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: 250,
+    }
+  }
+};
+
+const BootstrapInput = withStyles((theme: Theme) =>
+  createStyles({
+    input: {
+      borderRadius: 18,
+      position: 'relative',
+      backgroundColor: theme.palette.background.paper,
+      border: '1px solid #dfe1e5',
+      fontSize: 14,
+      height: 18,
+      padding: '8px 12px',
+      '&:focus': {
+        borderRadius: 18,
+        backgroundColor: theme.palette.background.paper
+      }
+    }
+  })
+)(InputBase);
 
 export function LocationSelector(props: any) {
   return (
-    <Select
-      value={props.value}
-      onChange={(e) => props.onChange(e)}
-      displayEmpty
-      className='select-country'
-    >
-      { props.countries.map((country: any) => <MenuItem key={country.Slug} value={country.Slug}>{country.Country}</MenuItem>) }
-    </Select>
+    <div>
+      <Select
+        id="country-select"
+        value={props.value}
+        onChange={(e) => props.onChange(e.target.value)}
+        MenuProps={MenuProps}
+        input={<BootstrapInput />}
+      >
+        { props.countries.map((country: any) => <MenuItem key={country.Slug} value={country.Slug}>{countryToFlag(country.ISO2)} {country.Country}</MenuItem>) }
+      </Select>
+    </div>
   )
 }
