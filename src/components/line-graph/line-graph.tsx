9036c99127f93cdda5495ca659038663d2ba8072
Abhishek Piedy <abhishek.piedy@gmail.com>
2020-04-22 02:18:49 -0400

    change line graph component to handle country change

--- src/components/line-graph/line-graph.tsx
+++ src/components/line-graph/line-graph.tsx
@@ -15,53 +15,74 @@ export class LineGraph extends React.Component<LineGraphProps, {}> {
     this.chartRef = React.createRef();
   }
 
-  componentDidMount() {
+  setupChart = () => {
     const myChartRef = this.chartRef.current?.getContext("2d");
     
-    myChartRef &&
-    new Chart(myChartRef, {
-      type: "line",
-      data: {
-        labels: this.props.labels,
-        datasets: [
-          {
-            label: "New cases",
-            data: this.props.data,
-            pointBackgroundColor: '#4285f4',
-            borderColor: '#4285f4',
-            backgroundColor: '#EAF0FD',
-            lineTension: 0,
-            pointRadius: 0
-          }
-        ]
-      },
-      options: {
-        //Customize chart options
-        legend: {
-          display: false
-        },
-        scales: {
-          xAxes: [{
-            gridLines: {
-              display:false
+    if(myChartRef) {
+      let gradient = myChartRef.createLinearGradient(0, 0, 0, 200);
+      gradient.addColorStop(0, 'rgba(234, 240, 253, 1)');
+      gradient.addColorStop(1, 'rgba(234, 240, 253, 0)');
+
+      new Chart(myChartRef, {
+        type: "line",
+        data: {
+          labels: this.props.labels,
+          datasets: [
+            {
+              borderWidth: 2,
+              label: "New cases",
+              data: this.props.data,
+              pointBackgroundColor: '#4285f4',
+              borderColor: '#4285f4',
+              backgroundColor: gradient,
+              lineTension: 0,
+              pointRadius: 0
             }
-          }],
-          yAxes: [{
-            gridLines: {
-              display:false
-            }   
-          }]
+          ]
+        },
+        options: {
+          //Customize chart options
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                minRotation: 360
+              }
+            }],
+            yAxes: [{
+              gridLines: {
+                display:true,
+                color: '#f5f5f5'
+              },
+              ticks: {
+                maxTicksLimit: 6
+              }
+            }]
+          }
         }
-      }
-    });
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.setupChart();
+  }
+
+  componentDidUpdate() {
+    this.setupChart();
   }
 
   render() {
     return (
       <div className="graph-container">
         <canvas
-            id="myChart"
-            ref={this.chartRef}
+          id="myChart"
+          ref={this.chartRef}
         />
       </div>
     )
