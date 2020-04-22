e2648bc16466d0d2fee28b81537ba9fc05df6e7d
Abhishek Piedy <abhishek.piedy@gmail.com>
2020-04-22 02:20:10 -0400

    changes to new cases component to adapt to selector and graph implementaions

--- src/components/new-cases/new-cases.tsx
+++ src/components/new-cases/new-cases.tsx
@@ -3,14 +3,13 @@ import moment from 'moment';
 
 import './new-cases.css';
 import { LineGraph } from '../line-graph/line-graph';
-
-const AvailableCountries = [
-  "Argentina", "Australia", "United States of America"
-];
+import { LocationSelector } from '../location-selector/location-selector';
+import { countries } from '../../shared/countries';
 
 type NewCasesState = {
   chartLabels: string[];
   chartData: number[];
+  currentCountry: string;
 }
 
 export class NewCases extends React.Component<{}, NewCasesState> {
@@ -20,20 +19,9 @@ export class NewCases extends React.Component<{}, NewCasesState> {
     super(props);
     this.state = {
       chartLabels: [],
-      chartData: []
+      chartData: [],
+      currentCountry: ''
     }
-  }
-
-  fetchCountries = () => {
-    fetch('https://api.covid19api.com/countries')
-      .then(res => res.json())
-      .then((data) => {
-        const filteredData = data.filter((x: any) => AvailableCountries.indexOf(x.Country));
-        const sortedData = filteredData.sort((a: any, b: any) => a.Country.localeCompare(b.Country));
-
-        this.fetchCountryData(sortedData[0].Slug);
-      })
-      .catch((err) => console.log(err))
   }
 
   fetchCountryData = (slug: string) => {
@@ -45,24 +33,38 @@ export class NewCases extends React.Component<{}, NewCasesState> {
   }
 
   computeChartData = (data: any) => {
-    const dayBeforeStart = this.startDate.subtract(1, 'days');
+    const dayBeforeStart = moment(this.startDate).subtract(1, 'd');
     const filteredCaseData = data.filter((x: any) => moment(x.Date).isSameOrAfter(dayBeforeStart)).map((x: any) => ({cases: x.Confirmed, date: x.Date}));
     const newCaseData = ((data: any[]) => {
+      let sixFromCurrent = moment(this.startDate).add(6, 'd');
       let x = [];
       for (let i = 1; i < data.length; i++) {
-        x.push({newCases: data[i].cases - data[i - 1].cases, date: data[i].date})
+        const dateLabel = moment(data[i].date).isBefore(sixFromCurrent) ? '' : data[i].date;
+        if (moment(data[i].date).isSame(sixFromCurrent)) sixFromCurrent.add(6, 'd');
+        x.push({newCases: data[i].cases - data[i - 1].cases, date: dateLabel});
       }
       return x;
     })(filteredCaseData);
-    
+
     this.setState({
-      chartLabels: newCaseData.map(x => moment(x.date).utc().format('MMM D')),
+      chartLabels: newCaseData.map(x => (x.date !== '') ? moment(x.date).utc().format('MMM D') : x.date),
       chartData: newCaseData.map(x => x.newCases)
     });
   }
 
+  handleCountryChange = (slug: string) => {
+    this.setState({
+      currentCountry: slug
+    });
+    this.fetchCountryData(slug);
+  }
+
   componentDidMount() {
-    this.fetchCountries();
+    const sortedCountries = countries.sort((a: any, b: any) => a.Country.localeCompare(b.Country));
+    this.setState({
+      currentCountry: sortedCountries[0].Slug
+    });
+    this.fetchCountryData(sortedCountries[0].Slug);
   }
 
   render() {
@@ -72,10 +74,13 @@ export class NewCases extends React.Component<{}, NewCasesState> {
           New cases
         </div>
 
-        {this.state.chartLabels.length &&
-        <div className="graph">
-          <LineGraph labels={this.state.chartLabels} data={this.state.chartData} />
-        </div>}
+        <div className="new-cases-content">
+          <div className="selector">
+            <LocationSelector value={this.state.currentCountry} onChange={(slug: string) => this.handleCountryChange(slug)} countries={countries} />
+          </div>
+
+          { this.state.chartLabels.length && <LineGraph labels={this.state.chartLabels} data={this.state.chartData} /> }
+        </div>
       </div>
     )
   }
